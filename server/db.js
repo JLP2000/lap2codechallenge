5f700f1b79a55829f881f4c0c9ac0dad3023a6c0
Adam-Dowsett <78181424+Adam-Dowsett@users.noreply.github.com>
2022-12-07 17:41:43 +0000

    something

---
+++ server/db.js
@@ -0,0 +1,45 @@
+const {MongoClient} = require("mongodb");
+
+
+async function main() {
+    const uri = "mongodb+srv://AndJ:password0015@telegraph.mzinr02.mongodb.net/?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+
+        await create(client, {
+            name: "lovely loft",
+            summary: "summary",
+            bedrooms: 1,
+            bathrooms: 1
+        })
+    }
+    catch(err){
+        console.error(err)
+    }
+    finally{
+        await client.close();
+    }
+
+}
+
+
+main().catch(console.error);
+
+async function create(client, newData) {
+    const result = await client.db("sample_airbnb").collection("listingAndReviews").insertOne(newData);
+    
+    console.log(`new listing has id of ${result.insertedId}`);
+}
+
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
